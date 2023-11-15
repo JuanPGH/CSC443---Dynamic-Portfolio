3b50025c876cb079d993526c231af04d8d89e423
Juan Pablo <jpghantous2002@outlook.com>
2023-11-15 23:15:50 +0200

    created login page and added icons

---
+++ pages/login.php
@@ -0,0 +1,44 @@
+<html>
+  <head>
+    <title>Template</title>
+    <link rel="stylesheet" href="../css/main.css" />
+    <link rel="stylesheet" href="../css/login.css" />
+  </head>
+  <body>
+    <div id="header">
+      <div id="title">
+        <span class="header-text">Login</span>
+      </div>
+      <div class="header-button">
+        <img src="../images/icons/home.png" />
+      </div>
+      <div class="header-button">
+        <a href="home.php"><img src="../images/icons/home.png" /></a>
+      </div>
+    </div>
+    <div id="content"></div>
+    <div id="footer">
+      <div id="social-medias">
+        <a href="https://www.twitter.com" target="_blank"
+          ><img src="../images/icons/twitter.png"
+        /></a>
+        <a
+          href="https://open.spotify.com/user/y74hmya3dklwhy5e2zdbx1uiu?si=fc0a43d5e50a417a"
+          target="_blank"
+          ><img src="../images/icons/spotify.png"
+        /></a>
+        <a href="https://instagram.com/juanpghantoush" target="_blank"
+          ><img src="../images/icons/instagram.png"
+        /></a>
+        <a href="https://www.linkedin.com/in/juanghantous/" target="_blank"
+          ><img src="../images/icons/linkedin.png"
+        /></a>
+      </div>
+      <div id="course-text">
+        <span class="footer-text">
+          CSC443 - Dynamic Portfolio - Fall 2023
+        </span>
+      </div>
+    </div>
+  </body>
+</html>
